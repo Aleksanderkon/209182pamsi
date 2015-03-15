dfad23146e3952afd398b0282cc09f9c6d9d1531
Aleksanderkon <209182@student.pwr.wroc.pl>
2015-03-15 13:56:54 +0000

    Revert "Lab-1"
    This reverts commit 9581a77047674455f18a71f7177cc55ab844bf23.

--- Lab-1/main.cpp
+++ Lab-1/main.cpp
@@ -7,10 +7,12 @@ using namespace std;
 int main()
 {
 	tablica T;
-	T.rozmiar = 10000000;
+
+	T.rozmiar = 1000;
     T.tab = new int[T.rozmiar];
+	
 	T.GeneratorLiczb();
-	T.MierzenieCzasu();	
+	T.MierzenieCzasu();
 	system("pause");
 	return 0;
 }
